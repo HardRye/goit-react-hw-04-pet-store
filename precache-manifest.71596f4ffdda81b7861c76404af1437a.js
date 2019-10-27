e653b2c947b929faa325b369b3f0fe7d337a83a9
HardRye <nikolaimihail@gmail.com>
2019-10-27 09:50:56 +0200

    Updates

---
+++ precache-manifest.71596f4ffdda81b7861c76404af1437a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c149af75b7db20f6d716bca4351eafb",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "aaf9e2d939a1d1240515",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.c70c536b.chunk.css"
+  },
+  {
+    "revision": "1af7a18d056aa823af32",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.c98ed158.chunk.js"
+  },
+  {
+    "revision": "aaf9e2d939a1d1240515",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.42581a4a.chunk.js"
+  },
+  {
+    "revision": "911313b77b6cc8e3fc26",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.8874a67c.js"
+  }
+]);
